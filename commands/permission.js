d48b72965fbc5ce75fd84ae89cd4bc20ec50a735
filip culig <filip.culig@gmail.com>
2020-12-25 19:07:17 +0100

    Added bare permission command

---
+++ commands/permission.js
@@ -0,0 +1,22 @@
+module.exports = {
+    name: 'permission',
+    description: 'Setup which members can use which commands.',
+    guildOnly: true,
+    args: true,
+    usage: '<action> <command-name> <role-that-can-execute>',
+    execute(message, args) {
+        if (args.length === 3) {
+            console.log(args)
+
+            let rolemap = message.guild.roles.cache
+                .sort((a, b) => b.position - a.position)
+                .map(r => r)
+                .join(",");
+            if (!rolemap) rolemap = "No roles";
+            console.log(rolemap);
+            return 0;
+        }
+
+        message.channel.send('Wrong number of arguments! Usage: **!' + this.name + ' ' + this.usage + '**');
+    },
+};
